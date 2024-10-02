a72fdc41a0634317b41f818faff9dc72827c24de
Adithya Deepthi Kumar <114129263+Adithyadk@users.noreply.github.com>
2024-10-02 15:36:47 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -20,8 +20,8 @@ public class Main {
     }
 
     if (!flag)
-      System.out.println(num + " is a prime number.");
+      System.out.println("The" + num + "is a prime number.");
     else
-      System.out.println(num + " is not a prime number.");
+      System.out.println("The" + num + " is not a prime number.");
   }
 }
